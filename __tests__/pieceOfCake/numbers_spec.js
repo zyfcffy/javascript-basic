d59a31cace6025029b8f4f259fdf7284e61a8872
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-07 09:04:16 +0800

    modify: hand in numbers_spec.jsp

--- __tests__/pieceOfCake/numbers_spec.js
+++ __tests__/pieceOfCake/numbers_spec.js
@@ -1,21 +1,21 @@
 describe('for numbers', () => {
-  it('should use real number calculation rather than integer in some languages', () => {
+  fit('should use real number calculation rather than integer in some languages', () => {
     const dividingResult = 3 / 4;
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = 0.75;
     // --end->
 
     expect(dividingResult).toEqual(expected);
   });
 
-  it('should be able to check if the number is NaN', () => {
+  fit('should be able to check if the number is NaN', () => {
     const notNumber = 0 / 0;
 
     // <--start
     // Please write an expression determine if `notNumber` is NaN.
-    const isNan = undefined;
+    const isNan = Number.isNaN(notNumber);
     // --end->
 
     expect(isNan).toBeTruthy();
